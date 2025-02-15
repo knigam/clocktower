10260443970f7d2e68f4627dd68ee62540ba39f5
Kushal Nigam <kushal.nigam1@gmail.com>
2025-02-15 14:26:56 -0500

    fixed assignRoles for new version of role-player

--- tests/clocktowerRules.test.ts
+++ tests/clocktowerRules.test.ts
@@ -1,5 +1,6 @@
 import { Player, RoleName } from "@knigam/role-player/src";
 import { clocktowerRules } from "../src/clocktowerRules";
+import { GameState } from "@knigam/role-player";
 
 test("assignRoles", () => {
   const players: Player[] = [1, 2, 3, 4, 5].map((i) => ({
@@ -7,7 +8,7 @@ test("assignRoles", () => {
     name: `Player ${i}`,
   }));
   const roles: RoleName[] = [];
-  const playersWithRoles = clocktowerRules.assignRoles(players, roles);
+  const playersWithRoles = clocktowerRules.assignRoles({ players } as GameState, roles);
   playersWithRoles.forEach((p) => {
     expect(p.role).toBeTruthy();
   });
